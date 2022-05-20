355de8c476186bdead17c0ce1c62b6b3ba1aa5d1
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-05-20 17:26:41 -0600

    fin funciones

--- app/src/main/java/com/example/holamundokotlin/MainActivity.kt
+++ app/src/main/java/com/example/holamundokotlin/MainActivity.kt
@@ -1,30 +1,23 @@
 package com.example.holamundokotlin
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
 data class Cursos(val nombre:String, val url: String)
-
 class MainActivity : AppCompatActivity() {
-
     val  react = Cursos("React","react")
     val kot = Cursos("Kotlin","kotlin")
     var cursoActual = react.copy()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val boton=findViewById(R.id.botoncito) as Button
         boton.setOnClickListener{
             view -> switchCurso(cursoActual)
         }
-
-
+        boton.setText(todosLosCursos())
     }
-
     fun switchCurso(curso:Cursos)
     {
         cursoActual= curso.copy()
@@ -35,13 +28,11 @@ class MainActivity : AppCompatActivity() {
             else -> print("Esto nunca va a pasar")
         }
         verEnPantalla ("Curso de ${cursoActual.nombre} en platzi.com/${cursoActual.url}")
-
     }
-
     fun verEnPantalla ( s :String)
     {
         val txt = findViewById(R.id.mensaje)as TextView
         txt.setText(s)
-
     }
+    fun todosLosCursos () : String = "${react.nombre} y ${kot.nombre}"
 }
