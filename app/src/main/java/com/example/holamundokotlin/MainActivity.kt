f2c062ee72d0ef47507e64c39ba90186a9efff2c
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-05-20 17:10:32 -0600

    cambios

--- app/src/main/java/com/example/holamundokotlin/MainActivity.kt
+++ app/src/main/java/com/example/holamundokotlin/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.holamundokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+data class Cursos(val nombre:String, val url: String)
 
 class MainActivity : AppCompatActivity() {
+
+    val  react = Cursos("React","react")
+    val kot = Cursos("Kotlin","kotlin")
+    var cursoActual = react.copy()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boton=findViewById(R.id.botoncito) as Button
+        boton.setOnClickListener{
+            view -> switchCurso(cursoActual)
+        }
+
+
+    }
+
+    fun switchCurso(curso:Cursos)
+    {
+        cursoActual= curso.copy()
+        when(curso.url)
+        {
+            "react" -> cursoActual= kot.copy()
+            "kotlin" -> cursoActual= react.copy()
+            else -> print("Esto nunca va a pasar")
+        }
+        verEnPantalla ("Curso de ${cursoActual.nombre} en platzi.com/${cursoActual.url}")
+
+    }
+
+    fun verEnPantalla ( s :String)
+    {
+        val txt = findViewById(R.id.mensaje)as TextView
+        txt.setText(s)
+
     }
 }
